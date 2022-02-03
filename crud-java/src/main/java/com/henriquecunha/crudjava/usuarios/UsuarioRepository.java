675cfe500fa54000efeaeac56cdb979b1d8343a0
Henrique Cunha <76274328+godrick05@users.noreply.github.com>
2022-02-03 02:04:03 -0300

    Delete crud-java directory

--- crud-java/src/main/java/com/henriquecunha/crudjava/usuarios/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.henriquecunha.crudjava.usuarios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "usuarios", path ="usuarios")
-public interface UsuarioRepository extends JpaRepository<Usuarios,Long>{
-	
-}
